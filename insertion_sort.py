fe0f6dd0ebbcb63a07cac2bc16319f50d737d4be
Irfan Ali <104905086+irfa110@users.noreply.github.com>
2025-02-06 22:46:20 +0530

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -15,3 +15,15 @@ def insertion_sort(list):
 list = [1, 5, 8, 7, 6, 4, 9, 3, 2, 10]
 insertion_sort(list)
 print(list)
+
+
+
+a = [13,46,24,52,20,9]
+
+n = len(a)
+for i in range(n):
+    j = i
+    while j > 0 and a[j - 1] > a[j]:
+        a[j-1], a[j] = a[j], a[j-1]
+        j -= 1
+print(a)
